fa6a030000fbb822067ca99aa01b897ad0fde8b7
Bobby Allen <bobbyallen.uk@gmail.com>
2013-03-18 20:29:54 +0000

    More changes to the Ubuntu 12.04 installer.

--- shellscripts/zpxrails-ubuntu1204-install.sh
+++ shellscripts/zpxrails-ubuntu1204-install.sh
@@ -17,3 +17,34 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
+# Lets install some required tools...
+apt-get update
+apt-get install build-essential python-software-properties libssl-dev libreadline-dev libcurl4-openssl-dev libssl-dev zlib1g-dev apache2-prefork-dev libapr1-dev libaprutil1-dev
+apt-get install libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt-dev
+
+# We now download the latest version of Ruby and build it...
+cd /usr/local/src
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz
+tar -z -xf ruby-1.9.3-p392.tar.gz
+cd ruby-1.9.3-392
+./configure --enable-shared
+make && sudo make install
+
+# We now download the latest version of Ruby Gems and install it...
+cd /usr/local/src
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
+tar xzvf rubygems-1.8.24.tgz
+cd rubygems-1.8.24
+ruby setup.rb
+gem install rubygems-update
+update_rubygems
+
+# Lets now install some Ruby Gems...
+
+# Install rails...
+gem install rails
+# Install bundler...
+gem install bundler
+# Install passenger...
+gem install passenger
+
