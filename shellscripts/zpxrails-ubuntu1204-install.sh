980b587d8d96c683afa9fb41f3023220ce91bf0d
Bobby Allen <bobbyallen.uk@gmail.com>
2013-03-18 20:17:41 +0000

    Changing install proceedure to use RVM instead!

--- shellscripts/zpxrails-ubuntu1204-install.sh
+++ shellscripts/zpxrails-ubuntu1204-install.sh
@@ -6,32 +6,14 @@
 #################################################################
 
 clear
-INSTALLLOG=~/zpxrails_install.log # We'll set a log file in the home directory!
-exec &>$INSTALLLOG # Lets pipe the output to the log file!
 
-echo -n "################################################################" >/dev/tty
-echo -n "# Rails Module for ZPanelX Installer                           #" >/dev/tty
-echo -n "# Created by: Bobby Allen (bobbyallen.uk@gmail.com)            #" >/dev/tty
-echo -n "################################################################" >/dev/tty
+echo -n "################################################################\n"
+echo -n "# Rails Module for ZPanelX Installer                           #\n"
+echo -n "# Created by: Bobby Allen (bobbyallen.uk@gmail.com)            #\n"
+echo -n "################################################################\n"
 
-echo -n "Installing some pre-requisites (build-essential, libssl-dev, libreadline-dev etc.)" >/dev/tty
-sudo apt-get install build-essential
-sudo apt-get install python-software-properties
-sudo apt-get install libssl-dev libreadline-dev
+if [ "$(id -u)" != "0" ]; then
+   echo "Sorry, this script must be run as root!" 1>&2
+   exit 1
+fi
 
-# Download and install (build Ruby on Rails)
-cd /usr/local/src
-sudo wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p392.tar.gz
-tar -z -xf ruby-1.9.3-p392.tar.gz
-cd ruby-1.9.3-p392
-sudo ./configure --enable-shared
-sudo make && sudo make install
-
-# Now we install and update Ruby Gems!
-cd /usr/local/src
-sudo wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
-sudo tar xzvf rubygems-1.8.24.tgz
-cd rubygems-1.8.24
-sudo ruby setup.rb
-sudo gem install rubygems-update
-sudo update_rubygems
