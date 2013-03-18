78ee887dca32d94db5e270d981424c37169f173b
Bobby Allen <bobbyallen.uk@gmail.com>
2013-03-18 19:50:21 +0000

    Adding more code to the first Ubuntu installer script, now updates and installs RubyGems and has a log file to log the install process.

--- shellscripts/zpxrails-ubuntu1204-install.sh
+++ shellscripts/zpxrails-ubuntu1204-install.sh
@@ -2,9 +2,19 @@
 
 #################################################################
 # Rails Module for ZPanelX Automated Installer for Ubuntu 12.04 #
-# Created by : Bobby Allen (bobbyallen.uk@gmail.com)            #
+# Created by: Bobby Allen (bobbyallen.uk@gmail.com)             #
 #################################################################
 
+clear
+INSTALLLOG=~/zpxrails_install.log # We'll set a log file in the home directory!
+exec &>$INSTALLLOG # Lets pipe the output to the log file!
+
+echo -n "################################################################" >/dev/tty
+echo -n "# Rails Module for ZPanelX Installer                           #" >/dev/tty
+echo -n "# Created by: Bobby Allen (bobbyallen.uk@gmail.com)            #" >/dev/tty
+echo -n "################################################################" >/dev/tty
+
+echo -n "Installing some pre-requisites (build-essential, libssl-dev, libreadline-dev etc.)" >/dev/tty
 sudo apt-get install build-essential
 sudo apt-get install python-software-properties
 sudo apt-get install libssl-dev libreadline-dev
@@ -16,3 +26,12 @@ tar -z -xf ruby-1.9.3-p392.tar.gz
 cd ruby-1.9.3-p392
 sudo ./configure --enable-shared
 sudo make && sudo make install
+
+# Now we install and update Ruby Gems!
+cd /usr/local/src
+sudo wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.24.tgz
+sudo tar xzvf rubygems-1.8.24.tgz
+cd rubygems-1.8.24
+sudo ruby setup.rb
+sudo gem install rubygems-update
+sudo update_rubygems
